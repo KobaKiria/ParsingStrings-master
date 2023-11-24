5d59d5cb108b2e3b05754f8a6dcf315afbebcb09
Thelion11 <kobakiria123@gmail.com>
2023-11-24 16:49:29 +0400

    updated

--- ParsingStrings/CharParser.cs
+++ ParsingStrings/CharParser.cs
@@ -31,7 +31,6 @@ namespace ParsingStrings
                 {
                     throw new ArgumentNullException(nameof(str));
                 }
-          
             }
             catch (FormatException)
             {
